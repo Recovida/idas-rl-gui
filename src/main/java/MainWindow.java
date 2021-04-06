617543485b60192114e09f333aaf96d6a9101cf3
Vinícius <matosvb@gmail.com>
2021-04-06 11:39:09 -0300

    Adição de algumas abas

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -18,9 +18,14 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -38,6 +43,9 @@ public class MainWindow {
     private JTextField textField_3;
     private JTextField textField_4;
     private JTextField textField_5;
+    private JTable table;
+    private JTextField textField_6;
+    private JTextField textField_7;
 
     /**
      * Launch the application.
@@ -89,18 +97,25 @@ public class MainWindow {
         tabbedPane.addTab("Datasets", null, datasetsTabPanel, null);
         GridBagLayout gbl_datasetsTabPanel = new GridBagLayout();
         gbl_datasetsTabPanel.columnWidths = new int[] { 200, 100, 200 };
-        gbl_datasetsTabPanel.rowHeights = new int[] { 20, 50, 50, 50, 0 };
+        gbl_datasetsTabPanel.rowHeights = new int[] { 10, 20, 30, 30, 30, 30 };
         gbl_datasetsTabPanel.columnWeights = new double[] { 1.0, 0.0, 1.0 };
         gbl_datasetsTabPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0,
-                1.0 };
+                0.0, 1.0 };
         datasetsTabPanel.setLayout(gbl_datasetsTabPanel);
+
+        Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
+        GridBagConstraints gbc_rigidArea = new GridBagConstraints();
+        gbc_rigidArea.insets = new Insets(0, 0, 5, 5);
+        gbc_rigidArea.gridx = 1;
+        gbc_rigidArea.gridy = 0;
+        datasetsTabPanel.add(rigidArea, gbc_rigidArea);
 
         JLabel lblNewLabel_1 = new JLabel("A");
         GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
         gbc_lblNewLabel_1.weightx = 1.0;
         gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
         gbc_lblNewLabel_1.gridx = 0;
-        gbc_lblNewLabel_1.gridy = 0;
+        gbc_lblNewLabel_1.gridy = 1;
         datasetsTabPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
         JLabel lblNewLabel_2 = new JLabel("B");
@@ -108,7 +123,7 @@ public class MainWindow {
         gbc_lblNewLabel_2.weightx = 1.0;
         gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
         gbc_lblNewLabel_2.gridx = 2;
-        gbc_lblNewLabel_2.gridy = 0;
+        gbc_lblNewLabel_2.gridy = 1;
         datasetsTabPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
 
         JPanel panel = new JPanel();
@@ -116,7 +131,7 @@ public class MainWindow {
         gbc_panel.insets = new Insets(0, 0, 5, 5);
         gbc_panel.fill = GridBagConstraints.HORIZONTAL;
         gbc_panel.gridx = 0;
-        gbc_panel.gridy = 1;
+        gbc_panel.gridy = 2;
         datasetsTabPanel.add(panel, gbc_panel);
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
@@ -135,7 +150,7 @@ public class MainWindow {
         GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
         gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
         gbc_lblNewLabel_4.gridx = 1;
-        gbc_lblNewLabel_4.gridy = 1;
+        gbc_lblNewLabel_4.gridy = 2;
         datasetsTabPanel.add(lblNewLabel_4, gbc_lblNewLabel_4);
 
         JPanel panel_3 = new JPanel();
@@ -143,7 +158,7 @@ public class MainWindow {
         gbc_panel_3.insets = new Insets(0, 0, 5, 0);
         gbc_panel_3.fill = GridBagConstraints.HORIZONTAL;
         gbc_panel_3.gridx = 2;
-        gbc_panel_3.gridy = 1;
+        gbc_panel_3.gridy = 2;
         datasetsTabPanel.add(panel_3, gbc_panel_3);
         panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
 
@@ -163,7 +178,7 @@ public class MainWindow {
         gbc_textField.insets = new Insets(0, 0, 5, 5);
         gbc_textField.fill = GridBagConstraints.HORIZONTAL;
         gbc_textField.gridx = 0;
-        gbc_textField.gridy = 2;
+        gbc_textField.gridy = 3;
         datasetsTabPanel.add(textField, gbc_textField);
         textField.setColumns(10);
 
@@ -171,7 +186,7 @@ public class MainWindow {
         GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
         gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
         gbc_lblNewLabel_5.gridx = 1;
-        gbc_lblNewLabel_5.gridy = 2;
+        gbc_lblNewLabel_5.gridy = 3;
         datasetsTabPanel.add(lblNewLabel_5, gbc_lblNewLabel_5);
 
         textField_1 = new JTextField();
@@ -179,7 +194,7 @@ public class MainWindow {
         gbc_textField_1.insets = new Insets(0, 0, 5, 0);
         gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
         gbc_textField_1.gridx = 2;
-        gbc_textField_1.gridy = 2;
+        gbc_textField_1.gridy = 3;
         datasetsTabPanel.add(textField_1, gbc_textField_1);
         textField_1.setColumns(10);
 
@@ -188,7 +203,7 @@ public class MainWindow {
         gbc_textField_2.insets = new Insets(0, 0, 5, 5);
         gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
         gbc_textField_2.gridx = 0;
-        gbc_textField_2.gridy = 3;
+        gbc_textField_2.gridy = 4;
         datasetsTabPanel.add(textField_2, gbc_textField_2);
         textField_2.setColumns(10);
 
@@ -197,7 +212,7 @@ public class MainWindow {
         gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
         gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
         gbc_lblNewLabel_6.gridx = 1;
-        gbc_lblNewLabel_6.gridy = 3;
+        gbc_lblNewLabel_6.gridy = 4;
         datasetsTabPanel.add(lblNewLabel_6, gbc_lblNewLabel_6);
 
         textField_3 = new JTextField();
@@ -205,19 +220,152 @@ public class MainWindow {
         gbc_textField_3.insets = new Insets(0, 0, 5, 0);
         gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
         gbc_textField_3.gridx = 2;
-        gbc_textField_3.gridy = 3;
+        gbc_textField_3.gridy = 4;
         datasetsTabPanel.add(textField_3, gbc_textField_3);
         textField_3.setColumns(10);
 
         Component verticalGlue = Box.createVerticalGlue();
         GridBagConstraints gbc_verticalGlue = new GridBagConstraints();
+        gbc_verticalGlue.fill = GridBagConstraints.VERTICAL;
         gbc_verticalGlue.insets = new Insets(0, 0, 0, 5);
         gbc_verticalGlue.gridx = 1;
-        gbc_verticalGlue.gridy = 4;
+        gbc_verticalGlue.gridy = 5;
         datasetsTabPanel.add(verticalGlue, gbc_verticalGlue);
+
+        JPanel panel_4 = new JPanel();
+        tabbedPane.addTab("Options", null, panel_4, null);
+        GridBagLayout gbl_panel_4 = new GridBagLayout();
+        gbl_panel_4.columnWidths = new int[] { 0, 0, 0 };
+        gbl_panel_4.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+        gbl_panel_4.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+        gbl_panel_4.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0,
+                Double.MIN_VALUE };
+        panel_4.setLayout(gbl_panel_4);
+
+        Component rigidArea_1 = Box.createRigidArea(new Dimension(20, 20));
+        GridBagConstraints gbc_rigidArea_1 = new GridBagConstraints();
+        gbc_rigidArea_1.insets = new Insets(0, 0, 5, 5);
+        gbc_rigidArea_1.gridx = 0;
+        gbc_rigidArea_1.gridy = 0;
+        panel_4.add(rigidArea_1, gbc_rigidArea_1);
+
+        JLabel lblNewLabel_3 = new JLabel("Linkage location");
+        GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+        gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_3.gridx = 0;
+        gbc_lblNewLabel_3.gridy = 1;
+        panel_4.add(lblNewLabel_3, gbc_lblNewLabel_3);
+
+        JPanel panel_5 = new JPanel();
+        GridBagConstraints gbc_panel_5 = new GridBagConstraints();
+        gbc_panel_5.fill = GridBagConstraints.BOTH;
+        gbc_panel_5.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_5.gridx = 1;
+        gbc_panel_5.gridy = 1;
+        panel_4.add(panel_5, gbc_panel_5);
+        panel_5.setLayout(new BoxLayout(panel_5, BoxLayout.X_AXIS));
+
+        textField_6 = new JTextField();
+        panel_5.add(textField_6);
+        textField_6.setColumns(10);
+
+        JButton btnNewButton_2 = new JButton("Select...");
+        btnNewButton_2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        panel_5.add(btnNewButton_2);
+
+        SpinnerNumberModel minScoreModel = new SpinnerNumberModel(1.0, 0.0,
+                Double.MAX_VALUE, 0.01);
+
+        SpinnerNumberModel maxRowsModel = new SpinnerNumberModel(2000000000, 0,
+                2000000000, 1);
+
+        JLabel lblNewLabel_7 = new JLabel("Index location");
+        GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+        gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_7.gridx = 0;
+        gbc_lblNewLabel_7.gridy = 2;
+        panel_4.add(lblNewLabel_7, gbc_lblNewLabel_7);
+
+        JPanel panel_6 = new JPanel();
+        GridBagConstraints gbc_panel_6 = new GridBagConstraints();
+        gbc_panel_6.fill = GridBagConstraints.BOTH;
+        gbc_panel_6.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_6.gridx = 1;
+        gbc_panel_6.gridy = 2;
+        panel_4.add(panel_6, gbc_panel_6);
+        panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
+
+        textField_7 = new JTextField();
+        panel_6.add(textField_7);
+        textField_7.setColumns(10);
+
+        JButton btnNewButton_3 = new JButton("Select...");
+        btnNewButton_3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        panel_6.add(btnNewButton_3);
+
+        JLabel lblNewLabel_8 = new JLabel("Minimum score");
+        GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+        gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_8.gridx = 0;
+        gbc_lblNewLabel_8.gridy = 3;
+        panel_4.add(lblNewLabel_8, gbc_lblNewLabel_8);
+        JSpinner spinner = new JSpinner(minScoreModel);
+        GridBagConstraints gbc_spinner = new GridBagConstraints();
+        gbc_spinner.anchor = GridBagConstraints.WEST;
+        gbc_spinner.insets = new Insets(0, 0, 5, 0);
+        gbc_spinner.gridx = 1;
+        gbc_spinner.gridy = 3;
+        panel_4.add(spinner, gbc_spinner);
+
+        JSpinner.NumberEditor minScoreNumberEditor = new JSpinner.NumberEditor(
+                spinner, "0.0000000");
+        minScoreNumberEditor.setPreferredSize(new Dimension(117, 21));
+        minScoreNumberEditor.setRequestFocusEnabled(false);
+        spinner.setEditor(minScoreNumberEditor);
+
+        JLabel lblNewLabel_9 = new JLabel("Only read first rows (A)");
+        GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
+        gbc_lblNewLabel_9.insets = new Insets(0, 0, 0, 5);
+        gbc_lblNewLabel_9.gridx = 0;
+        gbc_lblNewLabel_9.gridy = 4;
+        panel_4.add(lblNewLabel_9, gbc_lblNewLabel_9);
+        JSpinner spinner_1 = new JSpinner(maxRowsModel);
+        GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
+        gbc_spinner_1.anchor = GridBagConstraints.WEST;
+        gbc_spinner_1.gridx = 1;
+        gbc_spinner_1.gridy = 4;
+        panel_4.add(spinner_1, gbc_spinner_1);
+
+        JSpinner.NumberEditor maxRowsNumberEditor = new JSpinner.NumberEditor(
+                spinner_1, "0");
+        spinner_1.setEditor(maxRowsNumberEditor);
 
         JPanel panel_1 = new JPanel();
         tabbedPane.addTab("Linkage columns", null, panel_1, null);
+        panel_1.setLayout(new BorderLayout(0, 0));
+
+        table = new JTable();
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.addColumn("Number");
+        model.addColumn("ID");
+        model.addColumn("Type");
+        model.addColumn("Weight");
+        model.addColumn("Phon. weight");
+        model.addColumn("Column name");
+        model.addColumn("Renamed");
+        model.addColumn("Column name");
+        model.addColumn("Renamed", new Object[] { "item1" });
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel_1.add(scrollPane, BorderLayout.CENTER);
 
         JPanel panel_2 = new JPanel();
         tabbedPane.addTab("Extra columns", null, panel_2, null);
@@ -250,6 +398,9 @@ public class MainWindow {
         JLabel lblNewLabel = new JLabel("New label");
         lblNewLabel.setFont(new Font("Dialog", Font.ITALIC, 12));
         menuBar.add(lblNewLabel);
+
+        Component rigidArea_2 = Box.createRigidArea(new Dimension(20, 20));
+        menuBar.add(rigidArea_2);
     }
 
 }
